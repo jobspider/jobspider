1979700a82abc8901954bc75c901e94dc7fdfa60
Auditore F <951165464@qq.com>
2019-05-29 18:56:45 +0800

    添加项目文件。

---
+++ JobSpider/JobSpiderDlg.h
@@ -0,0 +1,34 @@
+﻿
+// JobSpiderDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CJobSpiderDlg 对话框
+class CJobSpiderDlg : public CDialogEx
+{
+// 构造
+public:
+	CJobSpiderDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_JOBSPIDER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
