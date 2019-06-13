bc1ac71ddefcb635893d9354657049ba2fa62e00
lenovo12345 <644230415@qq.com>
2019-06-13 13:41:16 +0800

    添加项目文件。

---
+++ MFCApplication3/Dialog1.cpp
@@ -0,0 +1,34 @@
+﻿// Dialog1.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "MFCApplication3.h"
+#include "Dialog1.h"
+#include "afxdialogex.h"
+
+
+// Dialog1 对话框
+
+IMPLEMENT_DYNAMIC(Dialog1, CDialogEx)
+
+Dialog1::Dialog1(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+Dialog1::~Dialog1()
+{
+}
+
+void Dialog1::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(Dialog1, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Dialog1 消息处理程序
---
+++ MFCApplication3/dialog3.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+
+// dialog3 对话框
+
+class dialog3 : public CDialogEx
+{
+	DECLARE_DYNAMIC(dialog3)
+
+public:
+	dialog3(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~dialog3();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG3 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
